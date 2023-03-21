c3a929c4ce220faa98a3540658857fc262d1a845
Abbas <tundeeey@gmail.com>
2023-03-21 18:46:02 +0100

    I just replaced my previous files with my calculator content

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+function show(num) {
+    let display = document.getElementById('display');
+    if (display.value === '0') {
+        display.value = num;
+    } else {
+        display.value += num;
+    }    
+}
+
+function clr(screen) {
+    let clear = document.getElementById('display');
+    clear.value = screen;
+}
+
+function cal() {
+    let answer = document.getElementById('display');
+    answer.value = eval(answer.value);
+}
