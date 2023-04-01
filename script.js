1aacbd0cd79c3a81733ec6824d37314f515f6844
Abbas <tundeeey@gmail.com>
2023-04-01 17:48:02 +0100

    I wrote the function operatorMinus() and also fix the histroy display screen to always display the last answer after each computation

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
 let display = document.getElementById('display');
 let input = document.getElementById("input");
-function show(num) {    
+let history = document.getElementById('history');
+let output = 0;
+function show(num) {
     if (display.value === '0') {
         display.value = num;
+        history.value = "Ans= " + eval(output);
+    }
+    else if (history.value === (output + " = ")) {
+        display.value = num;
+        history.value = "Ans= " + eval(output);
     }
     else {
         display.value += num;
+        history.value = "Ans= " + eval(output);
     }
+
+
 }
 
 function clr(screen) {
     display.value = screen;
+    history.value = "Ans= " + eval(output);
 }
 
 function cal() {
-    display.value = eval(display.value);
-    console.log(display.value);
-    // document.getElementById('history').value = show(input.value) + " = ";
+    output = display.value;
+    display.value = eval(output);
+    history.value = output + " = ";
+
+    console.log(display.value === eval(display.value));
+    // console.log(history.value === (output + " = "));
+
 }
 
 function operator(join) {
+    history.value = "Ans= " + eval(output);
+    display.value += join;
+}
+function operatorMinus(join) {
+    if (display.value === '0') {
+        display.value = join;
+        history.value = "Ans= " + eval(output);
+    }
+    else {
+        history.value = "Ans= " + eval(output);
+        display.value += join;
+    }
+
+}
+
+function dot(join) {
+    history.value = "Ans= " + eval(output);
     display.value += join;
 }
 
