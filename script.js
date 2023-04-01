a2f5e604be929a29a8610eb0be9e7db22ae5b66d
Abbas <tundeeey@gmail.com>
2023-04-01 18:22:17 +0100

    I was able to fix the AC button to change to CE onclick of any number and also give each of them their respective function, AC clears the screen to 0 while CE deletes number one by one from the back

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
 let display = document.getElementById('display');
 let input = document.getElementById("input");
 let history = document.getElementById('history');
+let ac = document.getElementById('ac');
 let output = 0;
 function show(num) {
     if (display.value === '0') {
         display.value = num;
         history.value = "Ans= " + eval(output);
+        ac.innerHTML = 'CE';
     }
     else if (history.value === (output + " = ")) {
         display.value = num;
         history.value = "Ans= " + eval(output);
+        ac.innerHTML = 'CE';
     }
     else {
         display.value += num;
         history.value = "Ans= " + eval(output);
+        ac.innerHTML = 'CE';
     }
 
 
 }
 
 function clr(screen) {
-    display.value = screen;
-    history.value = "Ans= " + eval(output);
+    if (ac.innerHTML === "CE" && display.value.length === 1) {
+        display.value = 0;
+    } 
+    else if (ac.innerHTML === "CE") {
+        display.value = display.value.slice(0,-1);
+    }
+    
+    else {
+        display.value = screen;
+        history.value = "Ans= " + eval(output);
+    }
+
 }
 
 function cal() {
     output = display.value;
     display.value = eval(output);
     history.value = output + " = ";
-
-    console.log(display.value === eval(display.value));
-    // console.log(history.value === (output + " = "));
-
+    ac.innerHTML = "AC";
 }
 
 function operator(join) {
